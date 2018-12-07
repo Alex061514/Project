797d7ebac7f6511b9c30a5144e6483cf1ee7ac5c
Alex061514 <44450708+Alex061514@users.noreply.github.com>
2018-12-06 20:56:30 -0600

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -6,16 +6,13 @@ else {
     $user = $result->fetch_assoc();
 
     if ( password_verify($_POST['password'], $user['password']) ) {
-        
         $_SESSION['email'] = $user['email'];
-
-
 		$_SESSION['logged_in'] = true;
-		header("location: profile.php");
+			header("location: profile.php");
     }
     else {
         $_SESSION['text'] = "email or password is incorrect!";
-        header("location: error.php");
+        	header("location: error.php");
     }
 }
 
